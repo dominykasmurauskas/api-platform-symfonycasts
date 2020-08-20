3e8015fb359664281a7400cd4d8b4ced7c7d3f2f
unknown <dominykasmurauskas1@gmail.com>
2020-08-20 14:31:51 +0300

    [E11] SerializedName & Constructor Args

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -8,6 +8,7 @@ use Carbon\Carbon;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 
 /**
  * @ApiResource(
@@ -61,9 +62,10 @@ class CheeseListing
      */
     private $isPublished = false;
 
-    public function __construct()
+    public function __construct(string $title = null)
     {
         $this->createdAt = new DateTimeImmutable();
+        $this->title = $title;
     }
 
     public function getId(): ?int
@@ -74,13 +76,6 @@ class CheeseListing
     public function getTitle(): ?string
     {
         return $this->title;
-    }
-
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
-
-        return $this;
     }
 
     public function getDescription(): ?string
@@ -98,6 +93,7 @@ class CheeseListing
     /**
      * The description of the listing
      *
+     * @SerializedName("description")
      * @Groups({"cheese_listing:write"})
      */
     public function setTextDescription(string $description): self
